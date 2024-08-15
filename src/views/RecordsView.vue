8cf9d7d9ba2c4e333c0ccf13cb7be97c4cf5eb43
Enger Jimenez <ejimenezr21@gmail.com>
2024-08-15 16:13:56 -0400

    Add heading to RecordsView

--- src/views/RecordsView.vue
+++ src/views/RecordsView.vue
@@ -3,5 +3,17 @@ import RecordsTable from '@/components/records_table.vue'
 </script>
 
 <template>
-  <RecordsTable />
+  <div>
+    <v-row>
+      <v-col>
+        <h1>Records</h1>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <RecordsTable />
+      </v-col>
+    </v-row>
+  </div>
 </template>
